81a6a0ef7866209fa5fcdbcd3081cb5a6321b8fc
Tabassum <sairatabassum386@gmail.com>
2021-08-01 00:48:06 +0600

    commit

---
+++ JSON/index.js
@@ -0,0 +1,17 @@
+const data = require("./friends_data.json");
+const data1 = require("./students_data.json");
+// ACCESS
+// console.log(data);
+// console.log(data.friends[0]);
+// console.log(data.friends[0].Name);
+// console.log(data1.students[1].languages[0]);
+
+// MODIFY
+// data.friends[0].Name = "sara";
+// console.log(data);
+
+// DELETE
+// delete data.friends[0].Name;
+// console.log(data);
+
+//
