925b292f145d1df06333d8d241c062f0f2ef9e0c
Tabassum <sairatabassum386@gmail.com>
2022-08-18 15:03:39 +0600

    commit

--- 03-ASYNCHRONOUS-JS/index.js
+++ 03-ASYNCHRONOUS-JS/index.js
@@ -1,20 +1,63 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
 
-  superagent
-    .get(
-      `https://dog.ceo/api/breed/${data}/images/random
-  `
-    )
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
+//   superagent
+//     .get(
+//       `https://dog.ceo/api/breed/${data}/images/random
+//   `
+//     )
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       console.log(res.body.message);
 
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log('Random dog image saved');
-      });
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+
+//         console.log('Random dog image saved');
+//       });
+//     });
+// });
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('i could not find any file');
+
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('could not write file');
+
+      resolve('Success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((result) => {
+    console.log(`This ${result}`);
+    return superagent.get(`https://dog.ceo/api/breed/${result}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+
+    //     fs.writeFile('dog-img.txt', res.body.message, (err) => {
+    //       if (err) return console.log(err.message);
+    //       console.log('Random dog image saved');
+  })
+  .then(() => {
+    console.log('Random dog image saved');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
