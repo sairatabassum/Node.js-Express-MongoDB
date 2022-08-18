6159836c0faf518ba40c358ab7cbb028e24adcee
Tabassum <sairatabassum386@gmail.com>
2022-08-18 21:29:35 +0600

    commit

--- 03-ASYNCHRONOUS-JS/index.js
+++ 03-ASYNCHRONOUS-JS/index.js
@@ -46,12 +46,27 @@ const getDogPic = async () => {
     const result = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`This ${result}`);
 
-    const res = await superagent.get(
+    // const res = await superagent.get(
+    //   `https://dog.ceo/api/breed/${result}/images/random`
+    // );
+
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${result}/images/random`
     );
-    console.log(res.body.message);
 
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${result}/images/random`
+    );
+
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${result}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    // console.log(all);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved');
   } catch (err) {
     console.log(err);
