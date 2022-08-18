adf73810361dfbd13b232107bd3a4d535e64a510
Tabassum <sairatabassum386@gmail.com>
2022-08-18 20:09:48 +0600

    commit

--- 03-ASYNCHRONOUS-JS/index.js
+++ 03-ASYNCHRONOUS-JS/index.js
@@ -55,9 +55,33 @@ const getDogPic = async () => {
     console.log('Random dog image saved');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2. Ready';
 };
-getDogPic();
+
+(async () => {
+  try {
+    console.log('1. Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3. Done getting dogs pic');
+  } catch (err) {
+    console.log('ERROR!');
+  }
+})();
+
+/*
+console.log('1. Will get dog pics');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3. Done getting dogs pic');
+  })
+  .catch((err) => {
+    console.log('ERROR!');
+  });
+  */
 /*
 readFilePro(`${__dirname}/dog.txt`)
   .then((result) => {
