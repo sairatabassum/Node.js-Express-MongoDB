3f4d207f4c8bc472b2523ceff1836889416ff4c8
Tabassum <sairatabassum386@gmail.com>
2022-08-20 00:39:14 +0600

    commit

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -31,7 +31,7 @@ app.get('/api/v1/tours', (req, res) => {
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, res.body);
+  const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
   fs.writeFile(
