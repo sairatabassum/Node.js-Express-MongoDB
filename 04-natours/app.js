52e45ce7f276be82d55c5bc130ee8641fa994002
Tabassum <sairatabassum386@gmail.com>
2022-08-19 23:47:10 +0600

    commit

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -1,16 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  //   res.status(200).send('Hell from the server side');
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'natours' });
-});
+// app.get('/', (req, res) => {
+//   //   res.status(200).send('Hell from the server side');
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to endpoint');
+// app.post('/', (req, res) => {
+//   res.send('You can post to endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: { tours },
+  });
 });
 
 const port = 3000;
