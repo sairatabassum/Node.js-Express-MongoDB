e280097da64f34d4c3879423159e471a8931e8da
Saira Tabassum <52861859+sairatabassum@users.noreply.github.com>
2022-08-31 14:19:36 +0600

    Update server.js

--- 04-natours/server.js
+++ 04-natours/server.js
@@ -1,8 +1,39 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
 
 const app = require('./app');
-
 // console.log(app.get('env'));
 // console.log(process.env);
 
