cd0eeece3383b1bdbd597a8ef1d5a3d9153d24ac
Tabassum <sairatabassum386@gmail.com>
2022-09-01 20:14:27 +0600

    commit

--- 04-natours/controllers/tourController.js
+++ 04-natours/controllers/tourController.js
@@ -98,11 +98,19 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
