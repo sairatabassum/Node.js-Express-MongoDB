8f21f9ccb85b1137ef4dbc02657a5e8639848866
sairatabassum <sairatabassum386@gmail.com>
2022-09-04 01:18:32 -0700

    commit

--- 04-natours/models/tourModel.js
+++ 04-natours/models/tourModel.js
@@ -1,31 +1,47 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-        name: {
-          type: String,
-          required: [true, 'A tour must have a name'],
-          unique: true,
-        },
-        rating: {
-          type: Number,
-          default: 4.5,
-        },
-        price: {
-          type: Number,
-          required: [true, 'A tour must have a price'],
-        },
-      });
-const Tour=mongoose.model('Tour',tourSchema);
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, ' A tour must have a difficulty'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
-module.exports=Tour;
-  // const testTour=new Tour({
-  //   name:"The forest kiker",
-  //   rating:4.7,
-  //   price:497
-  // });
-  
-  // testTour.save().then(doc=>{
-  //   console.log(doc);
-  // }).catch(err=>{
-  //   console.log("ERROR");
-  // })
+module.exports = Tour;
+// const testTour=new Tour({
+//   name:"The forest kiker",
+//   rating:4.7,
+//   price:497
+// });
+
+// testTour.save().then(doc=>{
+//   console.log(doc);
+// }).catch(err=>{
+//   console.log("ERROR");
+// })
