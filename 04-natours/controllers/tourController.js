f514ff720613555cb6e2b18ca0dbb9c488dda470
sairatabassum <sairatabassum386@gmail.com>
2022-09-07 22:05:21 +0600

    commit

--- 04-natours/controllers/tourController.js
+++ 04-natours/controllers/tourController.js
@@ -52,7 +52,7 @@ class APIfeatures {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.query.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 
@@ -74,6 +74,18 @@ class APIfeatures {
     } else {
       this.query = this.query.select('-__v');
     }
+    return this;
+  }
+
+  pagination() {
+    // 4)PAGINATION
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=50, 1-10, page 1, 11-20, page 2, 21-30, page 3
+    this.query = this.query.skip(skip).limit(limit);
+
     return this;
   }
 }
@@ -132,21 +144,25 @@ exports.getAllTours = async (req, res) => {
     //   query = query.select('-__v');
     // }
 
-    // 4)PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+    // // 4)PAGINATION
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
-    // page=2&limit=50, 1-10, page 1, 11-20, page 2, 21-30, page 3
-    query = query.skip(skip).limit(limit);
+    // // page=2&limit=50, 1-10, page 1, 11-20, page 2, 21-30, page 3
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
     // EXECUTE QUERY
-    const features = new APIfeatures(Tour.find(), req.query).filter().sort();
+    const features = new APIfeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFileds()
+      .pagination(0);
     const tours = await features.query;
 
     // SEND RESPONSE
