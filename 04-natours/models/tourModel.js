1c5e70c034dd9aa213c30a1e73d0d143d5a3e9da
sairatabassum <sairatabassum386@gmail.com>
2022-09-12 21:35:06 +0600

    commit

--- 04-natours/models/tourModel.js
+++ 04-natours/models/tourModel.js
@@ -42,7 +42,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price; // 100 <  200
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
