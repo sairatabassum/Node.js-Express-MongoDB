f48bc50629d0603a53bce75a2572cdf76a710fb0
sairatabassum <sairatabassum386@gmail.com>
2022-09-12 20:12:28 +0600

    commit

--- 04-natours/models/tourModel.js
+++ 04-natours/models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal to 40 charcaters'],
+      minlength: [10, 'A tour name must have more or equal to 10 charcaters'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, ' A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, difficult, medium',
+      },
     },
     ratingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingQuantity: {
       type: Number,
