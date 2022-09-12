8d8f9ba2466868efb527a1d4c9af22c742d7dd20
sairatabassum <sairatabassum386@gmail.com>
2022-09-12 12:32:47 +0600

    COMMIT

--- 04-natours/models/tourModel.js
+++ 04-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -63,6 +65,28 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE : runs before .save() and .create()
+
+// Pre document middleware
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function (next) {
+  console.log('Will save document');
+  next();
+});
+
+// Post document middleware
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+});
+
+// QUERY MIDDLEWARE
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
