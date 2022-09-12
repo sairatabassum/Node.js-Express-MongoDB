94802eebf3d375e556cff702cb10df54cc2de5e9
sairatabassum <sairatabassum386@gmail.com>
2022-09-12 21:54:35 +0600

    commit

--- 04-natours/models/tourModel.js
+++ 04-natours/models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const valid
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -11,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal to 40 charcaters'],
       minlength: [10, 'A tour name must have more or equal to 10 charcaters'],
+      validate: [validator.isAlpha, 'Tour name must only contain  character'],
     },
     slug: String,
     duration: {
