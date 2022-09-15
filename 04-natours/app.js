b0c322913678c0e589363302a2fcf8a2b7a16a3d
sairatabassum <sairatabassum386@gmail.com>
2022-09-15 13:01:03 +0600

    commit

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -59,11 +59,11 @@ app.all('*', (req, res, next) => {
   //   status: 'fail',
   //   message: `Can't find ${req.originalUrl} on this server!!`,
   // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!!`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!`, 404));
 });
 
 app.use((err, req, res, next) => {
