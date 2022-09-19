2c8be2c8d5d1270cf4bf4ea6363b5f69c4971121
sairatabassum <sairatabassum386@gmail.com>
2022-09-19 21:28:47 +0600

    commit

--- 04-natours/controllers/authController.js
+++ 04-natours/controllers/authController.js
@@ -1,8 +1,15 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body); //u
+  // const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
