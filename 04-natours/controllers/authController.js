11aabd54c5925b6d919af691a6f51b8b668991f5
sairatabassum <sairatabassum386@gmail.com>
2022-09-25 19:49:25 +0600

    commit

--- 04-natours/controllers/authController.js
+++ 04-natours/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-const jwt = require('');
+const jwt = require('jsonwebtoken');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   // const newUser = await User.create(req.body);
@@ -11,10 +12,29 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body.email;
+
+  // 1) Check if email and password exit
+
+  if (!email || !password) {
+    next(new AppError('Please provide email and passowrd', 400));
+  }
+
+  // 2) Check if user exits & password is correct
+
+  // 3) If everything ok, send token to client
+};
