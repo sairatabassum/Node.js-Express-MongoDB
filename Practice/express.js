dfaaee61e295a187a64e622b57e7bb59e269000d
sairatabassum <sairatabassum386@gmail.com>
2022-11-02 22:45:34 +0600

    commit

--- Practice/express.js
+++ Practice/express.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // express.raw(), express.text()
 
 app.get("/", (req, res) => {
   res.send("this is home page");
