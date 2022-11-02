9ab558aa7bffc0e3de05e6be8e07cc03c36a1855
sairatabassum <sairatabassum386@gmail.com>
2022-11-02 22:15:02 +0600

    commit

---
+++ Practice/express.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("this is home page");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("this is home page with post method");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
