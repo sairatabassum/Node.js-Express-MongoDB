b2185103ba99f26d6a0b0cdae751020647b549b4
sairatabassum <sairatabassum386@gmail.com>
2022-11-05 15:41:36 +0600

    commit

--- Practice/request.js
+++ Practice/request.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
+const adminRoute = express.Router();
+
+adminRoute.get("/dashboard", (req, res) => {
+  //   console.log(req.baseUrl);
+  //   console.log(req.originalUrl);
+  console.log(req.url);
+
+  res.send("We are in admin dashboard");
+});
+
+app.use("/admin", adminRoute);
+
 app.get("/user/:id", (req, res) => {
-  console.log(req.baseUrl);
+  //   console.log(req.baseUrl);
+  //   console.log(req.originalUrl);
+  console.log(req.url);
+
   res.send("hello");
 });
 
