2175915252b55cd62aa7e014435f8f1c60114eb6
sairatabassum <sairatabassum386@gmail.com>
2022-11-12 20:34:59 +0600

    commit

--- Practice/File upload with multer/index.js
+++ Practice/File upload with multer/index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 
 // File upload folder
 const UPLOADS_FOLDER = "upload/";
 
+// define the storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOADS_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    // Important File.pdf -> important-file-32864783468382.pdf
+    const fileExt = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(fileExt, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") +
+      "-" +
+      Date.now();
+    cb(null, fileName + fileExt);
+  },
+});
+
 // prepare the final multer upload object
 var upload = multer({
-  dest: UPLOADS_FOLDER,
+  storage: storage,
   limits: {
     fileSize: 1000000, //1MB
   },
