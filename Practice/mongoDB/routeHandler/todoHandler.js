aba072f0f656b6cadcc83986dcc761d615d7348d
sairatabassum <sairatabassum386@gmail.com>
2022-11-16 22:44:00 +0600

    commit

--- Practice/mongoDB/routeHandler/todoHandler.js
+++ Practice/mongoDB/routeHandler/todoHandler.js
@@ -7,5 +7,16 @@ router.get("/", async (req, res) => {});
 // GET A TODO by ID
 router.get("/:id", async (req, res) => {});
 
-// GET ALL THE TODOS
-router.post("/:id", async (req, res) => {});
+// POST TODO
+router.post("/", async (req, res) => {});
+
+// POST MULTIPLE TODO
+router.post("/all", async (req, res) => {});
+
+// PUT TODO
+router.put("/:id", async (req, res) => {});
+
+// DELETE TODO
+router.delete("/:id", async (req, res) => {});
+
+module.exports = router;
