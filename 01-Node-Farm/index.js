d9713c3a6b017f6bf933fa51c7b23e67dc4e0b67
saira <sairatabassum386@gmail.com>
2023-08-28 18:49:24 +0600

    commit

--- 01-Node-Farm/index.js
+++ 01-Node-Farm/index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// const hello = 'hello world';
-// console.log(hello);
+const hello = 'hello world';
+console.log(hello);
 
 ////////////////////////////////////////////////////////////////////////////////
 // ******Reading and Writing Files******* ( Blocking, synchronous way)
